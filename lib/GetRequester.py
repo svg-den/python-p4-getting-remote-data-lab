c9eed4bcab0e068c5b5299e0d8e0ecf316b6633b
svg-den <sundaywanjiru@gmail.com>
2024-04-05 11:45:31 +0300

    remote data

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,27 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
-
+        """
+        Sends a GET request to the URL and returns the response body as text.
+        If the request is unsuccessful or raises an error, None is returned.
+        """
+        response = requests.get(self.url)
+        return response.content
+   
     def load_json(self):
-        pass
+        """
+        Uses get_response_body to get the response in text format,
+        then attempts to convert and return it as a Python dictionary or list.
+        Returns None if conversion fails or response is invalid.
+        """
+        response_body = self.get_response_body()
+        if response_body:
+            try:
+                return json.loads(response_body)
+            except json.JSONDecodeError as e:
+                print(f"JSON decoding error: {e}")
+        return None
